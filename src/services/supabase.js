f001d7ac0edd96a211ff47d2e057fd4c2f817bd7
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2023-08-26 22:12:37 +0100

    Creating a data base with supabase then connecting it with the App

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://zpbiwjnuoimudhtffxwu.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpwYml3am51b2ltdWRodGZmeHd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMwNzM2MDAsImV4cCI6MjAwODY0OTYwMH0.yS30WSkSTZvPfUtNNmhoH_bCo-b3qceHYF86Zo5kwLM'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase;
