b306aa7b539471011ce9c460ee9d7d5cd6f78086
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2023-08-26 14:05:06 +0100

    Building the App Layout

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,28 @@ import Login from "./pages/Login";
 import Settings from "./pages/Settings";
 import Users from "./pages/Users";
 import PageNotFound from "./pages/PageNotFound";
-import GlobalStyles  from "./styles/GlobalStyles";
+import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
   return (
     <>
-    <GlobalStyles/>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Navigate replace to="dashboard" />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="account" element={<Account />} />
-        <Route path="booking" element={<Booking />} />
-        <Route path="cabins" element={<Cabins />} />
-        <Route path="login" element={<Login />} />
-        <Route path="settings" element={<Settings />} />
-        <Route path="users" element={<Users />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="account" element={<Account />} />
+            <Route path="booking" element={<Booking />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="users" element={<Users />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
