a9f6b6fd701c46caf2fbefeb6c71a92c91ea2d89
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2023-08-27 21:46:32 +0100

    Dispalaying Botification For delete error and Success

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -3,26 +3,35 @@ import Header from "./Header";
 import SideBar from "./SideBar";
 import { styled } from "styled-components";
 
-const StyledAppLayout=styled.div`
+const StyledAppLayout = styled.div`
   display: grid;
   grid-template-columns: 26rem 1fr;
   grid-template-rows: auto 1fr;
   height: 100vh;
-`
+`;
 
-const Main =styled.main`
+const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
-`
+`;
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
 
 function AppLayout() {
   return (
     <StyledAppLayout>
-      <Header/>
-      <SideBar/>
+      <Header />
+      <SideBar />
 
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyledAppLayout>
   );
