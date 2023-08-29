81f255bfb69fe349918dbdeaeb7fd9673b2de8a9
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2023-08-29 16:40:37 +0100

    Fetching Applications Settings

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,21 +1,29 @@
 import Form from '../../ui/Form';
-import FormRow from '../../ui/FormRow';
+import FormRow from '../cabins/FormRow';
 import Input from '../../ui/Input';
+import { useSettings } from './useSettings';
 
 function UpdateSettingsForm() {
+  const {isLoading,settings}=useSettings()
+  const {
+    minBookingLength,
+    maxBookingLength,
+    maxGuestsPerBooking,
+    breakfastPrice
+  }=settings;
   return (
     <Form>
       <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+        <Input type='number' disabled={isLoading} id='min-nights' value={minBookingLength}/>
       </FormRow>
       <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+        <Input type='number'  disabled={isLoading}id='max-nights'  value={maxBookingLength}/>
       </FormRow>
       <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+        <Input type='number' disabled={isLoading} id='max-guests'  value={maxGuestsPerBooking}/>
       </FormRow>
       <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+        <Input type='number' disabled={isLoading} id='breakfast-price' value={breakfastPrice} />
       </FormRow>
     </Form>
   );
