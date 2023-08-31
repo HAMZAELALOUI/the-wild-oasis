b60feb0797a88e12a120c2b1a65fd297df4904ec
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2023-08-31 15:05:08 +0100

    Converting the Modal to a Compound Component

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,24 +1,44 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
+import CabinTable from "./CabinTable";
 
 function AddCabin() {
-  const [showModal, setShowModal] = useState(false);
-
   return (
-    <div>
-      <Button onClick={() => setShowModal((show) => !showModal)}>
-        Add New Cabin
-      </Button>
-      {showModal && (
-        <Modal onClose={() => setShowModal((show) => !showModal)}>
-          {" "}
-          <CreateCabinForm  onCloseModal={() => setShowModal((show) => !showModal)}/>
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new Cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+      
+      <Modal.Open opens='table'>
+        <Button>Show Cabin </Button>
+      </Modal.Open>
+      <Modal.Window name='table'>
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
   );
 }
+
+// function AddCabin() {
+//   const [showModal, setShowModal] = useState(false);
+
+//   return (
+//     <div>
+//       <Button onClick={() => setShowModal((show) => !showModal)}>
+//         Add New Cabin
+//       </Button>
+//       {showModal && (
+//         <Modal onClose={() => setShowModal((show) => !showModal)}>
+//           {" "}
+//           <CreateCabinForm  onCloseModal={() => setShowModal((show) => !showModal)}/>
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
 
 export default AddCabin;
