9253498515d8ee073bae8e65f91af3941daef533
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2023-09-01 23:08:43 +0100

    Client-Side Filtering: Filtering Cabins

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,23 +1,19 @@
-
-import { useState } from "react";
 import CabinTable from "../features/cabins/CabinTable";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
-import Button from "../ui/Button";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import AddCabin from "../features/cabins/AddCabin";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 
 function Cabins() {
-
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter/Sort</p>
+        <CabinTableOperations />
       </Row>
       <Row>
-        <CabinTable/>
-          <AddCabin/>
+        <CabinTable />
+        <AddCabin />
       </Row>
     </>
   );
