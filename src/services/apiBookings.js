587edf26eb0a64e503ebf8a1ebd4eb85825e635d
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2023-09-02 23:45:39 +0100

    API-Side Sorting: Sorting Bookings

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -7,6 +7,9 @@ export async function getBookings({ filter, sortBy }) {
   //FILTER
   if (filter !== null) query = query[filter.method || 'eq'](filter.field, filter.value);
 
+  //SortBy 
+  if (sortBy) query = query.order(sortBy.field, { ascending: sortBy.direction === 'asc' });
+
   const { data, error } = await query;
   if (error) {
     console.error(error);
