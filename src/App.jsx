751fd05ce2e6dce71182bfa7b62677a5debd2225
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2023-09-04 13:02:01 +0100

    Building the Single Booking Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Account from "./pages/Account";
-import Booking from "./pages/Bookings";
+import Bookings from "./pages/Bookings";
+import Booking from "./pages/Booking";
 import Cabins from "./pages/Cabins";
 import Login from "./pages/Login";
 import Settings from "./pages/Settings";
@@ -34,7 +35,8 @@ function App() {
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="account" element={<Account />} />
-            <Route path="bookings" element={<Booking />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="bookings/:bookingId" element={<Booking />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="settings" element={<Settings />} />
             <Route path="users" element={<Users />} />
