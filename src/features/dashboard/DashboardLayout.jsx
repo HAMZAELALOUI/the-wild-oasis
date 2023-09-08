1008321aa33ae24735555d6289f02714ad2527dd
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2023-09-08 19:05:19 +0100

    Displaying a Pie Chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useCabins } from "../cabins/useCabins";
 import Spinner from "../../ui/Spinner";
 import Stats from "./Stats";
 import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -32,8 +33,8 @@ function DashboardLayout() {
         cabinCount={cabins.length}
       />
       <div>Todays activities</div>
-      <div>Chart stay duration</div>
-      <SalesChart bookings={bookings} numDays={numDays} /> 
+      <DurationChart confirmedStays={confirmedStays} />
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
